aac3d6daf4ee966ede4156099bab1040f572843b
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-27 18:41:46 -0800

    Creating branch

--- AsyncInn/Startup.cs
+++ AsyncInn/Startup.cs
@@ -15,6 +15,7 @@ namespace AsyncInn
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,16 @@ namespace AsyncInn
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(route =>
+            {
+                route.MapRoute(
+                    name: "default"
+                    template: "{controller=Home}/{action=index}/{id?}"
+                    );
+            });
 
             app.Run(async (context) =>
             {
